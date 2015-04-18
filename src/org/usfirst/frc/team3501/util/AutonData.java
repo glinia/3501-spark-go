f86bcb4376227b9c86981b23b13d821d7be37531
Logan Howard <logan@oflogan.com>
2015-04-17 22:16:53 -0700

    remove stupid file thing. will replace with smartdashboard probably

--- src/org/usfirst/frc/team3501/util/AutonData.java
+++ src/org/usfirst/frc/team3501/util/AutonData.java
@@ -1,9 +1,5 @@
 package org.usfirst.frc.team3501.util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class AutonData {
@@ -38,31 +34,7 @@ public class AutonData {
     }
 
     private void populate() {
-        String file;
-
-        try {
-            file = readConfigFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-            populateDefaults();
-            return;
-        }
-
-        try {
-            Arrays.stream(file.split("\n"))
-            .map(line -> line.split(" "))
-            .forEach((action) -> {
-                double speed = Double.parseDouble(action[0]);
-                double time  = Double.parseDouble(action[1]);
-                String name  = action[2];
-
-                speeds.put(name, speed);
-                times.put(name, time);
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-            populateDefaults();
-        }
+        populateDefaults();
     }
 
     private void populateDefaults() {
@@ -76,23 +48,5 @@ public class AutonData {
         times.put("drive_over_step",  1.2);
         times.put("drive_past_step",  1.5);
         times.put("pickup_container", 1.4);
-    }
-
-    private String readConfigFile() throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(
-                "auton_times_and_speeds.conf"));
-
-        StringBuilder sb = new StringBuilder();
-
-        in.readLine(); // get rid of first line
-
-        String curLine;
-        while ((curLine = in.readLine()) != null)
-            sb.append(curLine + "\n");
-        String finalString = sb.toString();
-
-        in.close();
-
-        return finalString;
     }
 }
